0ca058209716d2302fc80782a11e126fec33a0c7
niharikamanhar <niharikamanhar476@gmail.com>
2025-06-07 17:20:53 +0530

    uploading changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "../styles/Projects.css"
 function Card(props) {
+  console.log(props.image);
   return (
     <div className="project">
       <div className="subheading">{props.title}</div>
-      <img src={props.image} alt="project 1" className="project-image" />
+      <img src={props.image} alt="project 1" />
       <div className="project-description">{props.description}</div>
       <a
         href={props.githubLink}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Projects.css"
-import image1 from "../assets/3.jpg"; // Replace with your actual image path
+// import image1 from "../assets/3.jpg"; // Replace with your actual image path
 import Card from "./Card";
 import projectData from "../assets/projects.json"; // Assuming you have a JSON file with project data
 
@@ -11,7 +11,7 @@ function Projects() {
       <div className="project-container">
         {projectData.map((project)=>{
           return (
-            <Card key={project.id} title={project.title} image={project.image} description={project.description} githubLink={project.githubLink} />
+            <Card key={project.id} title={project.title} image={project.image} description={project.description} githubLink={project.url} />
           )
         })}
       </div>
