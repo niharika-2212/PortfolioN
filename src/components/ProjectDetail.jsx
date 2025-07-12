84d199ed9059aeb528c7f2660c21098e82f6d3eb
niharika <niharikamanhar476@gmail.com>
2025-07-12 22:47:12 +0530

    updating projects

---
+++ src/components/ProjectDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProjectDetail(){
+    return(
+        <div className="project-detail">
+            project
+        </div>
+    )
+}
+
+export default ProjectDetail;
